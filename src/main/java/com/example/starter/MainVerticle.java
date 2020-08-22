08c839ae6a921e199412a1788aecf9b5bf045a31
gcnyin <guchounongyin@gmail.com>
2020-08-22 15:48:20 +0800

    add router example

--- src/main/java/com/example/starter/MainVerticle.java
+++ src/main/java/com/example/starter/MainVerticle.java
@@ -1,28 +1,26 @@
 package com.example.starter;
 
 import io.vertx.core.AbstractVerticle;
-import io.vertx.core.Future;
 import io.vertx.core.Promise;
-import io.vertx.core.http.HttpServerRequest;
+import io.vertx.core.http.HttpServerResponse;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
 
 public class MainVerticle extends AbstractVerticle {
   @Override
   public void start(Promise<Void> startPromise) {
+    Router router = Router.router(vertx);
+    router.get("/hello").handler(this::handler);
     vertx.createHttpServer()
-      .requestHandler(this::getEnd)
-      .listen(8888, http -> {
-        if (http.succeeded()) {
-          startPromise.complete();
-          System.out.println("HTTP server started on port 8888");
-        } else {
-          startPromise.fail(http.cause());
-        }
-      });
+      .requestHandler(router)
+      .listen(8888);
   }
 
-  private Future<Void> getEnd(HttpServerRequest req) {
-    return req.response()
-      .putHeader("content-type", "application/json")
-      .end("{\"status\": \"success\"}");
+  private void handler(RoutingContext routingContext) {
+    // This handler will be called for every request
+    HttpServerResponse response = routingContext.response();
+    response.putHeader("content-type", "text/plain");
+    // Write to the response and end it
+    response.end("Hello World from Vert.x-Web!");
   }
 }
