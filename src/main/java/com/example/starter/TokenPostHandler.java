2bcb287d91063a1c54d4141ae48ea1edbcbd08df
gcnyin <guchounongyin@gmail.com>
2020-08-23 22:28:51 +0800

    using RxJava3 to wrap MongoDB result

--- src/main/java/com/example/starter/TokenPostHandler.java
+++ src/main/java/com/example/starter/TokenPostHandler.java
@@ -2,17 +2,15 @@ package com.example.starter;
 
 import com.mongodb.BasicDBObject;
 import com.mongodb.reactivestreams.client.MongoCollection;
+import io.reactivex.rxjava3.core.Flowable;
 import io.vertx.core.Handler;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.RoutingContext;
 import lombok.extern.slf4j.Slf4j;
 import org.bson.Document;
 import org.reactivestreams.Publisher;
-import org.reactivestreams.Subscriber;
-import org.reactivestreams.Subscription;
 
 import java.util.Objects;
-import java.util.concurrent.CompletableFuture;
 
 @Slf4j
 public class TokenPostHandler implements Handler<RoutingContext> {
@@ -35,46 +33,22 @@ public class TokenPostHandler implements Handler<RoutingContext> {
     var password = body.getString("password");
     var isValidFuture = validate(username, password);
 
-    isValidFuture.thenAccept(isValid -> {
+    isValidFuture.subscribe(isValid -> {
       if (!isValid) {
         var message = new JsonObject().put("message", "invalid username or password").encode();
         response.setStatusCode(401).end(message);
         return;
       }
       var token = jwtUtils.generate(username);
-      log.info("generated token");
       response.end(new JsonObject().put("token", token).encode());
     });
   }
 
-  private CompletableFuture<Boolean> validate(String username, String password) {
+  private Flowable<Boolean> validate(String username, String password) {
     MongoCollection<Document> collection = dbClient.getDatabase().getCollection("user");
     BasicDBObject condition = new BasicDBObject().append("username", username);
     Publisher<Document> user = collection.find(condition).first();
-
-    CompletableFuture<Document> documentFuture = new CompletableFuture<>();
-
-    user.subscribe(new Subscriber<>() {
-      @Override
-      public void onSubscribe(Subscription subscription) {
-        subscription.request(1);
-      }
-
-      @Override
-      public void onNext(Document document) {
-        log.info("get document from mongodb");
-        documentFuture.complete(document);
-      }
-
-      @Override
-      public void onError(Throwable throwable) {
-        log.error("onError", throwable);
-      }
-
-      @Override
-      public void onComplete() {
-      }
-    });
-    return documentFuture.thenApply(document -> Objects.equals(password, document.getString("password")));
+    return Flowable.fromPublisher(user)
+      .map(document -> Objects.equals(password, document.getString("password")));
   }
 }
