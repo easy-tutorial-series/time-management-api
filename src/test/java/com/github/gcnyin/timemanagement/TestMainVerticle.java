a5f0dd7b5cf43cecf2ba6c9bce500e26f60052a6
gcnyin <guchounongyin@gmail.com>
2020-08-25 23:50:30 +0800

    add testing placeholder

---
+++ src/test/java/com/github/gcnyin/timemanagement/TestMainVerticle.java
@@ -0,0 +1,24 @@
+package com.github.gcnyin.timemanagement;
+
+import io.vertx.core.Vertx;
+import io.vertx.junit5.VertxExtension;
+import io.vertx.junit5.VertxTestContext;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+@Slf4j
+@ExtendWith(VertxExtension.class)
+class TestMainVerticle {
+  @BeforeEach
+  void deploy_verticle(Vertx vertx, VertxTestContext testContext) {
+    vertx.deployVerticle(new MainVerticle(), testContext.succeeding(id -> testContext.completeNow()));
+  }
+
+  @Test
+  void name(Vertx vertx, VertxTestContext testContext) {
+    log.info("test vertx");
+    testContext.completeNow();
+  }
+}
