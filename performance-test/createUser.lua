461dc0af1d2941b5aec6bca0a2327bcb61a784a9
gcnyin <guchounongyin@gmail.com>
2020-09-26 09:51:54 +0800

    empty project

--- performance-test/createUser.lua
+++
@@ -1,3 +0,0 @@
-wrk.method = "POST"
-wrk.body = '{"username": "username", "password": "password"}'
-wrk.headers["Content-Type"] = "application/json"
--- src/main/java/com/github/gcnyin/timemanagement/exception/BadRequestException.java
+++
@@ -1,7 +0,0 @@
-package com.github.gcnyin.timemanagement.exception;
-
-public class BadRequestException extends RuntimeException {
-  public BadRequestException(String message) {
-    super(message);
-  }
-}
