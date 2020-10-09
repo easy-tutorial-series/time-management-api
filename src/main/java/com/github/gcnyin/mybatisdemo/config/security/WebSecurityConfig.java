9d4b0467ff74426495b5dfc65098362a0d94dbc1
gcnyin <guchounongyin@gmail.com>
2020-10-09 12:29:28 +0800

    set SessionCreationPolicy.STATELESS

--- src/main/java/com/github/gcnyin/mybatisdemo/config/security/WebSecurityConfig.java
+++ src/main/java/com/github/gcnyin/mybatisdemo/config/security/WebSecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -47,13 +48,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http
-      .authorizeRequests()
+      .httpBasic()
+      .and().authorizeRequests()
       .antMatchers("/heartbeat").permitAll()
       .anyRequest().authenticated()
-      .and().httpBasic()
       .and().addFilterBefore(bearerAuthenticationFilter(), BasicAuthenticationFilter.class)
-      .formLogin().disable()
-      .csrf().disable();
+      .formLogin().disable().csrf().disable()
+      .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
   }
 
   @Bean
